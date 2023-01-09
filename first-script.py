f9f6e2efaea485274573d72fccf18ee4ef972cf2
gcavdar93 <112583003+gcavdar93@users.noreply.github.com>
2023-01-09 13:58:18 +0300

    Delete first-script.py

--- first-script.py
+++
@@ -1,8 +0,0 @@
-from pymol import cmd
-from pymol import stored
-
-cmd.loadall("*.pdb")                   
-
-cmd.alter("lig*" + " and chain B", "chain='I'")     
-
-cmd.create("obj0_00","lig.000.00.pdb or rec.pdb") 
